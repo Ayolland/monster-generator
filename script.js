792beac14f32d14aa5cda50ab73883dd4d53dc78
Andrew Y <and.t.yoll@gmail.com>
2016-02-23 15:48:20 -0600

    mostly working, color math is broken after all

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function switchMonster(){
-	var monster_array = ["Snek", "Axedude", "Weregoat"];
-	var newMonster = monster_array[Math.floor(Math.random()*monster_array.length)];
+	var monster_array = ["Snek", "Axedude", "Weregoat", "Scamp", "Gooball", "Wiz", "Skelebones"];
 	var monsterElement = document.getElementById("monster");
+	var oldMonster = monsterElement.classList[1];
+	var newMonster = monster_array[Math.floor(Math.random()*monster_array.length)];
 	monsterElement.className = ( "monster " + newMonster );
+	var article = "a ";
+	if (oldMonster === newMonster){ article = "another ";}
+	document.getElementById("log").innerHTML = "It's "+ article + newMonster + "!";
+
 }
 
 document.getElementById('switchMonster').addEventListener("click", switchMonster);
+switchMonster();
